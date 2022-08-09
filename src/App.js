fb74d6ebbd4eb1c7ee1f6b19d7eef71d7e225fa8
danaDvlp <colleksql3@gmail.com>
2022-08-10 02:42:19 +0900

    Add Qustionmark Slide

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import "./App.css";
 import styled from "styled-components";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Mousewheel, Pagination } from "swiper";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 //components
 import { Mobile, Pc } from "./components/MediaQuery.jsx";
@@ -22,6 +24,9 @@ import ThirdProject from "./pages/ThirdProject.jsx";
 import FourthProject from "./pages/FourthProject.jsx";
 import FifthProject from "./pages/FifthProject.jsx";
 import SixthProject from "./pages/SixthProject.jsx";
+import { useState } from "react";
+
+AOS.init();
 
 function App() {
   return (
@@ -47,19 +52,24 @@ function MainIndex() {
           direction={"horizontal"}
           spaceBetween={30}
           pagination={{
-            clickable : true,
+            clickable: true
           }}
           mousewheel={true}
           modules={[Mousewheel, Pagination]}
           className="mySwiper"
         >
-          <SwiperSlide> <Introduce /> </SwiperSlide>
-          <SwiperSlide> <QuestionMark /> </SwiperSlide>
+          <SwiperSlide>
+            {" "}
+            <Introduce />{" "}
+          </SwiperSlide>
+          <SwiperSlide>
+            {" "}
+            <QuestionMark />{" "}
+          </SwiperSlide>
           <SwiperSlide>Slide 3</SwiperSlide>
           <SwiperSlide>Slide 4</SwiperSlide>
           <SwiperSlide>
-            Slide 5
-            <Link to="first-project">첫번째 프로젝트 이동 링크</Link>
+            Slide 5<Link to="first-project">첫번째 프로젝트 이동 링크</Link>
             <Link to="second-project">두번째 프로젝트 이동 링크</Link>
             <Link to="third-project">세번째 프로젝트 이동 링크</Link>
             <Link to="fourth-project">네번째 프로젝트 이동 링크</Link>
@@ -69,36 +79,55 @@ function MainIndex() {
           <SwiperSlide>Slide 6</SwiperSlide>
         </Swiper>
       </Pc>
-      <Mobile>        
-      </Mobile>
+      <Mobile></Mobile>
     </>
   );
 }
 
-function Introduce() {
-  return(
-    <>
-      <div className="introCon">
+class Introduce extends React.Component {
+  state = { x: 0, y: 0 };
+  handleMouseMove = (event) => {
+    this.setState({
+      x: event.pageX,
+      y: event.pageY
+    });
+  };
+  render() {
+    const { x, y } = this.state;
+
+    return (
+      <section className="introCon">
         <div className="introBox">
-          <p className="titleCon">
+          <h1 className="titleCon" onMouseMove={this.handleMouseMove}>
             <span className="title">사용성을 개선을 위해</span>
-            <strong className="title englishTitle">cirtical thinking</strong>
-            <span className="title"> 하는 개발자 심단비입니다.</span>
-          </p>
+            <strong
+              className="title englishTitle"
+            >
+              cirtical thinking {x} {y}
+            </strong>
+            <span className="title">하는 개발자 심단비입니다.</span>
+          </h1>
         </div>
-      </div>
-    </>
-  );
+      </section>
+    );
+  }
 }
 
 function QuestionMark() {
-  return(
-    <>
-      <div>
-        
+  return (
+    <section className="secBg">
+      <h2 className="explainTxt">
+        <span>퍼즐 맞추듯</span>
+        <span>
+          <strong>세밀</strong>하게
+        </span>
+        <span>개발합니다</span>
+      </h2>
+      <div className="questionMark">
+        <img src={`${process.env.PUBLIC_URL}/img/svg/puzzle_whole.svg`} />
       </div>
-    </>
-  )
+    </section>
+  );
 }
 
 export default App;
